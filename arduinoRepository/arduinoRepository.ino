c8f91948820f65402807db63fc9c4f005f9c8de5
Marcin Motowidlo <marcin.motowidlo@gmail.com>
2023-01-15 18:32:18 +0100

    lcd display / humidity sensor

--- arduinoRepository/arduinoRepository.ino
+++ arduinoRepository/arduinoRepository.ino
@@ -1,8 +1,15 @@
 #include <OneWire.h>
 #include <DS18B20.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <string.h>
 
 // sensor Port
 #define ONEWIRE_PIN 2
+#define LCD_DISPLAY_ADDRESS 0x27
+#define HUMIDITY_SENSOR_PORT A0
+
+LiquidCrystal_I2C lcd(LCD_DISPLAY_ADDRESS, 16, 2);
 
 bool debugFeaturesEnabled = true;
 int sensorReadIntervalMs = 1000;
@@ -21,8 +28,9 @@ DS18B20 sensors(&onewire);
 void setup() {
   Serial.begin(9600);
   Serial.write("Serial port initialized");
-  sensors.begin();
-  sensors.request(temperatureSensorAddress);
+
+  setupLcd();
+  setupSensors();
 
   // debug features:
   if (debugFeaturesEnabled) {
@@ -32,10 +40,31 @@ void setup() {
 
 void loop() {
   if (sensors.available()) {
-    float temperature = sensors.readTemperature(temperatureSensorAddress);
 
-    Serial.print(temperature);
-    Serial.println(" 'C");
+    char lcdBuffer[16];      // LCD line 1 buffer
+    char tempBuffer[4];      // temp value buffer
+    char humidityBuffer[4];  // humidity value buffer
+
+    // read temperature:
+    float temperature = sensors.readTemperature(temperatureSensorAddress);
+    Serial.print("Temperature ['C]: ");
+    Serial.println(temperature);
+    
+    // read humidity:
+    int humiditySensor = analogRead(HUMIDITY_SENSOR_PORT);
+    float humidityVoltage = humiditySensor * (5.0 / 1023.0);
+    float humidityPercentage = ((5 - humidityVoltage) / 5) * 100;
+    Serial.print("Humidity [%]: ");
+    Serial.println(humidityPercentage);
+
+    // display results:
+    dtostrf(temperature, 4, 2, tempBuffer);
+    snprintf(lcdBuffer, 16, "Temp: %s 'C", tempBuffer);
+    writeLcd(0, lcdBuffer);
+
+    dtostrf(humidityPercentage, 4, 2, humidityBuffer);
+    snprintf(lcdBuffer, 16, "Humi: %s %%", humidityBuffer);
+    writeLcd(1, lcdBuffer);
 
     sensors.request(temperatureSensorAddress);
     delay(sensorReadIntervalMs);
@@ -73,4 +102,19 @@ byte* identifyOneWireDevices() {
     }
     return address;
   }
+}
+
+void setupLcd() {
+  lcd.begin();
+  lcd.backlight();
+}
+
+void setupSensors() {
+  sensors.begin();
+  sensors.request(temperatureSensorAddress);
+}
+
+void writeLcd(int line, String text) {
+  lcd.setCursor(0, line);
+  lcd.print(text);
 }
